4da56145dc31e34cae57ac9632c112a552aea77e
sainaen <sainaen@gmail.com>
2014-01-05 14:14:41 +0200

    Remove setup-oh-my-zsh.sh script

--- scripts/setup-oh-my-zsh.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh-custom/plugins/zsh-syntax-highlighting
